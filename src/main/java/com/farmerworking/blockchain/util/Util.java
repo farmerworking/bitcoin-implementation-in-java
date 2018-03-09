ef4843674f316b7d5674fb42c543dc89404c4634
farmerworking <farmerworking@gmail.com>
2018-03-10 00:45:37 +0800

    help method

--- src/main/java/com/farmerworking/blockchain/util/Util.java
+++ src/main/java/com/farmerworking/blockchain/util/Util.java
@@ -29,4 +29,11 @@ public class Util {
         byte[] checksum = Util.checksum(Bytes.concat(arrays));
         return Base58.encode(Bytes.concat(Bytes.concat(arrays), checksum));
     }
+
+    public static String toBinary(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length * Byte.SIZE);
+        for( int i = 0; i < Byte.SIZE * bytes.length; i++ )
+            sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
+        return sb.toString();
+    }
 }
